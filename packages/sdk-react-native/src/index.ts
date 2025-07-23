d50de0e5fba492df89eb98fa87fde34c84504539
Austin <austin@orga-ai.com>
2025-07-23 13:06:27 +0200

    Update version to 0.0.0-test.4 and enhance exports in sdk-react-native

--- packages/sdk-react-native/src/index.ts
+++ packages/sdk-react-native/src/index.ts
@@ -1,5 +1,5 @@
 export * from "../../shared";
-export {OrgaAI} from "../../shared"
+export {OrgaAI, ORGAAI_MODELS, ORGAAI_VOICES } from "../../shared"
 export {useOrgaAI } from "./hooks/useOrgaAI";
 export { OrgaAIProvider, useOrgaAIContext } from "./hooks/OrgaAIProvider";
 export * from "./types";
